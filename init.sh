39dba36640c50d842d0b35f1268847a48150c404
0-programmer <76661512+0-programmer@users.noreply.github.com>
2025-08-29 12:01:47 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-LOG_FILE="$HOME/setup.log"
-
-# 1. открываем fd 5 на запись в лог
-exec 5>>"$LOG_FILE"
-
-# 2. указываем bash, что трассировка пойдёт в fd 5
-BASH_XTRACEFD=5
-
-# 3. включаем трассировку
-set -x
+if [[ "${1:-}" == "--debug" ]]; then
+  set -x
+fi
 
 #info messages
 BLUE="\033[1m\033[34m"
